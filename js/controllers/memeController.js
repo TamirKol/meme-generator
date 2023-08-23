4af46576bbf376b3fc37596cdf7225f3857de92c
TamirKol <77102783+TamirKol@users.noreply.github.com>
2023-08-23 21:05:11 +0300

    submit 2100

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -2,21 +2,25 @@
 let gElCanvas
 let gCtx
 let currentImg
+let currentColor
+let selectedLine
 
 //init
-function onInit(){
+function onInit() {
     renderGallery()
- gElCanvas= document.querySelector('canvas')
- gCtx= gElCanvas.getContext('2d')
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
 }
 
-function renderMeme(){
+function renderMeme() {
     onClearCanvas()
     currentImg
     coverCanvasWithImage(currentImg)
-    const elMeme=onGetMeme(currentImg.id)
-    console.log(elMeme);
-    drawText(elMeme.lines[elMeme.selectedLineIdx].txt)
+    const elMeme = onGetMeme(currentImg.id)
+    const elMemeLine = elMeme.lines[elMeme.selectedLineIdx]
+    console.log(elMeme)
+    const elMemeSelectedLine=selectedLine
+    drawText(elMemeLine.txt, elMemeLine.color,elMemeLine.size,elMemeSelectedLine)
 
 
 }
@@ -24,43 +28,79 @@ function renderMeme(){
 function coverCanvasWithImage(elImg) {
     gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
     gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-    
+
 }
 
 function onClearCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
     // We may clear part of the canvas
     // gCtx.clearRect(0, 0, gElCanvas.width / 2, gElCanvas.height / 2)
-  }
+}
 //draw
-function drawText(text, x, y) {
-    console.log(gElCanvas.height,gElCanvas.width/2);
-    gCtx.lineWidth = 2
+function drawText(text, color = 'white', size = 30,selectedLine, x, y) {
+    console.log(gElCanvas.height, gElCanvas.width / 2);
+    gCtx.lineWidth = 1
     gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '30px Arial'
+    gCtx.fillStyle = color
+    gCtx.font = size + 'px Arial'
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
-  
-    gCtx.fillText(text, gElCanvas.height/2,50)
-    gCtx.strokeText(text, gElCanvas.height/2,50)
-  }
+    switch(selectedLine){
+        case 1:
+            gCtx.fillText(text, gElCanvas.height / 2, 50)
+            gCtx.strokeText(text, gElCanvas.height / 2, 50)
+            break;
+        case 2:
+            gCtx.fillText(text, gElCanvas.height /2, gElCanvas.width / 2)
+            gCtx.strokeText(text, gElCanvas.height / 2, gElCanvas.width / 2)
+            break;
+        }
+    // gCtx.fillText(text, gElCanvas.height / 2, 50)
+    // gCtx.strokeText(text, gElCanvas.height / 2, 50)
+}
 
 //get
-function onGetMeme(imgId){
-const meme=getMeme(imgId)
-return meme
+function onGetMeme(imgId) {
+    const meme = getMeme(imgId)
+    return meme
 }
 //create
 
-//update
-function OnSetLineTxt(text){
-SetLineTxt(text)
-renderMeme()
+function onAddLine() {
+    setNewLine()
+
 }
-function onImgSelect(img){
+
+//update
+function OnSetLineTxt(text, lineNumber) {
+    setLineTxt(text, lineNumber)
+    selectedLine=lineNumber
+    console.log(selectedLine)
+    renderMeme()
+}
+function onImgSelect(img) {
     console.log(img);
     setImg(img)
-    currentImg=img
+    currentImg = img
     renderMeme()
+}
+
+function onSetColor(color) {
+    currentColor = setColor(color)
+    renderMeme()
+}
+
+function onTextSizeChange(diff) {
+    setTextSize(diff)
+    renderMeme()
+}
+//download
+
+function downloadCanvas(elLink) {
+    const dataUrl = gElCanvas.toDataURL()
+    console.log('dataUrl', dataUrl)
+
+    elLink.href = dataUrl
+    // Set a name for the downloaded file
+    elLink.download = 'my-meme'
 }
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -1,45 +1,75 @@
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
 var gMeme = {
- selectedImgId: 5,
- selectedLineIdx: 0,
- lines: [
- {
- txt: 'I sometimes eat Falafel',
- size: 20,
- color: 'red'
- }
- ]
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
 }
-var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
-function getMeme(imgIdx){
-    gMeme.selectedImgId=imgIdx
+function getMeme(imgIdx) {
+    gMeme.selectedImgId = imgIdx
     return gMeme
 }
 
-function SetLineTxt(text){
-    let selectedLine= gMeme.selectedLineIdx
-    gMeme.lines[selectedLine].txt=text
+function setLineTxt(text, lineNumber = 0) {
+    const lineIdx = lineNumber - 1
+    let selectedLine =lineIdx
+    gMeme.lines[selectedLine].txt = text
     console.log(gMeme);
 }
+function setNewLine() {
+    let linesNumber = gMeme.lines.length
 
-function setImg(img){
-   const imgUrl= img.src
-   const imgId= img.id
-   const inGImgs= gImgs.some(img=>img.url===imgUrl)
-   if(!inGImgs){
-   let img= _createMemeImg(imgId,imgUrl,)
-    gImgs.push(img)
-   }
- console.log(gImgs);
-    
+    console.log(gMeme.lines.length);
+    if (linesNumber >= 3) return -1
+    else {
+        let line = _createLine('change line text', 30, 'white')
+        gMeme.lines.push(line)
+        console.log('gmeme+ line', gMeme)
+        return gMeme.lines.length
+    }
 }
 
-function _createMemeImg(id,url,keywords=[]){
-    const meme={
+function setImg(img) {
+    const imgUrl = img.src
+    const imgId = img.id
+    const inGImgs = gImgs.some(img => img.url === imgUrl)
+    if (!inGImgs) {
+        let img = _createMemeImg(imgId, imgUrl,)
+        gImgs.push(img)
+    }
+    console.log(gImgs);
+
+}
+function setColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].color = color
+    console.log(gMeme)
+    return color
+}
+function setTextSize(diff) {
+    gMeme.lines[gMeme.selectedLineIdx].size += diff
+}
+
+function _createMemeImg(id, url, keywords = []) {
+    const meme = {
         id,
         url,
         keywords
     }
     return meme
+}
+function _createLine(text, size, color) {
+    const line = {
+        text,
+        size,
+        color
+    }
+    return line
+
 }
